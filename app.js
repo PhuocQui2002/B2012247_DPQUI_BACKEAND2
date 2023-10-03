1d677e33bc15c78c9b389d83cd275ad640cd9e55
King QuytU <quib2012247@student.ctu.edu.vn>
2023-10-03 14:40:50 +0700

    update

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ app.get("/", (req, res) => {
 });
 // handle 404 response
 app.use((req, res, next) => {
-  return next(new ApiError(404, " not found"));
+  return next(new ApiError(404, "Resource not found"));
 });
 // define error-handling middleware last, after other app.use() and routes calls
 app.use((err, req, res, next) => {
